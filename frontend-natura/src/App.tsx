3e5d8c826b54a9011ad4911fc645f63c0c4347e7
Jander da Costa Nery <jander.webmaster@gmail.com>
2024-08-02 19:35:14 -0400

    Feat: Create Initial ListProducts

--- frontend-natura/src/App.tsx
+++ frontend-natura/src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/header";
 import { Hero } from "./components/hero";
 import { Promotions } from "./components/promotions";
+import { SectionProductsHome } from "./components/sectionProductsHome";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Promotions />
       <Header />
       <Hero />
+      <SectionProductsHome />
     </>
   );
 }
