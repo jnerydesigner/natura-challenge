c65714ecc11f75948c257e4c255052a0c4169d12
Jander da Costa Nery <jander.webmaster@gmail.com>
2024-08-03 07:46:10 -0400

    Feat: Adjust in fonts

--- frontend-natura/src/components/sectionProductsHome.tsx
+++ frontend-natura/src/components/sectionProductsHome.tsx
@@ -4,7 +4,7 @@ export const SectionProductsHome = () => {
   return (
     <div className="w-8/12 h-[auto] bg-white relative flex justify-center items-center flex-col">
       <h2 className="my-8 text-[1.8rem] font-bold">
-        descubra as fragâncias que combinam com você
+        descubra as fragâncias que combinam com você Cosméticos&Co
       </h2>
       <div className="w-[100%] h-[auto] grid grid-cols-4 gap-1 px-12 py-8">
         <CardProductHome />
--- frontend-natura/tailwind.config.js
+++ frontend-natura/tailwind.config.js
@@ -1,49 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     extend: {
       fontFamily: {
-        display: 'Henriete Bold',
-      }
-    },
-    fontFamily: {
-      henriete: ["Henriete Bold"],
-      oldschool: ['Oldschool'],
-      sans: ["Merriweather"],
+        henriete: ["Henriete Bold"],
+        oldschool: ["Oldschool"],
+        merriweater: ["Merriweather"],
+        sans: ["Roboto Serif"],
+      },
     },
     colors: {
       gray: {
-        100: '#F0F0F0',
-        200: '#E8E8E8',
-        300: '#E0E0E0',
-        400: '#DCDCDC',
-        500: '#D0D0D0',
-        600: '#B0B0B0',
-        700: '#A8A8A8',
-        800: '#909090',
-        900: '#696969',    
-
+        100: "#F0F0F0",
+        200: "#E8E8E8",
+        300: "#E0E0E0",
+        400: "#DCDCDC",
+        500: "#D0D0D0",
+        600: "#B0B0B0",
+        700: "#A8A8A8",
+        800: "#909090",
+        900: "#696969",
       },
-      graylight: '#f0f0f0ff',
-      black: '#000000ff',
-      gold: '#ffc633ff',
-      white: '#ffffff',
-      inputGray: '#f0f0f0ff',
-      inputGrayDark: '#e0e0e0ff',
-      pinkLight:'#ffeaeaff',
-      pinkDark: '#ff6868ff'
-    }
+      graylight: "#f0f0f0ff",
+      black: "#000000ff",
+      gold: "#ffc633ff",
+      white: "#ffffff",
+      inputGray: "#f0f0f0ff",
+      inputGrayDark: "#e0e0e0ff",
+      pinkLight: "#ffeaeaff",
+      pinkDark: "#ff6868ff",
+    },
   },
   plugins: [],
-}
-
+};
