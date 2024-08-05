2e6ad65c3f6ad43867c748c2ad40e32800c892dd
Jander da Costa Nery <jander.webmaster@gmail.com>
2024-08-05 10:11:41 -0400

    Feat: test security products

--- backend-natura/src/presenters/product.controller.ts
+++ backend-natura/src/presenters/product.controller.ts
@@ -1,3 +1,4 @@
+import { Public } from '@application/decorators/public-request.decorator';
 import { ProductTypeormDto } from '@application/dtos/product-typeorm.dto';
 import { ProductService } from '@application/services/product.service';
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
@@ -7,6 +8,7 @@ export class productController {
   constructor(private readonly productService: ProductService) {}
 
   @Get()
+  @Public()
   findAll(@Query('page') page: number, @Query('limit') limit: number) {
     return this.productService.findAll(+page, +limit);
   }
